fedc9030d3a94275939d993351bc60acfa0a11d1
Vitaliy Z <vv-z@mail.ru>
2023-03-06 20:30:04 +0300

    new commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int b = 5000; // стоимость билета
+        int x = 20; // количество рублей для получения одной милли
+
+        int m = b/x;
+
+
+        System.out.println("Вам будет начислено " + m + " милль.");
+    }
+}
