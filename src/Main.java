c228680e93ae1b27803f666580591133cd04a8d5
Vitaliy Z <vv-z@mail.ru>
2023-03-07 07:42:12 +0300

    code editing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int b = 5000; // стоимость билета
-        int x = 20; // количество рублей для получения одной милли
+        int ticket_price = 5000; // стоимость билета
+        int ruble_to_milli = 20; // количество рублей для получения одной милли
 
-        int m = b/x;
+        int milli = ticket_price / ruble_to_milli;
 
 
-        System.out.println("Вам будет начислено " + m + " милль.");
+        System.out.println("Вам будет начислено " + milli + " милль.");
     }
 }
